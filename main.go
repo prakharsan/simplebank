3b1452954a4aef1c9fbd5c1891d580bfa5cc5112
Prakhar Sankrityayan <prakhar3099@gmail.com>
2023-01-10 20:38:46 +0530

    Lecture #12

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/lib/pq"
+	"github.com/prakharsan/simplebank/api"
+	db "github.com/prakharsan/simplebank/db/sqlc"
+	"github.com/prakharsan/simplebank/db/util"
+)
+
+func main() {
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config", err)
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
+	if err != nil {
+		log.Fatal("Cannot connect to db", err)
+	}
+
+	store := db.NewStore(conn)
+	server := api.NewServer(store)
+
+	err = server.Start(config.ServerAddress)
+	if err != nil {
+		log.Fatal("cannot start server", err)
+	}
+}
