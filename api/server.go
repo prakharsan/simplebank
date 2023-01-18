c19eb462f01ba19655cec791239c6f1b9d52258a
Prakhar Sankrityayan <prakhar3099@gmail.com>
2023-01-19 02:47:53 +0530

    Lecture #17

--- api/server.go
+++ api/server.go
@@ -26,6 +26,7 @@ func NewServer(store db.Store) *Server {
 	router.GET("/accounts/:id", server.getAccount)
 	router.GET("/accounts", server.listAccount)
 	router.POST("/transfers", server.CreateTransfer)
+	router.POST("/users", server.createUser)
 
 	server.router = router
 	return server
